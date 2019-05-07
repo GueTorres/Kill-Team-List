30a3f824d24a6ed7d72151bb0e55312bee60775f
GueTorres <guevara.torres619@outlook.com>
2019-05-07 14:35:24 -0400

    Update AdMechDriver.java
    making a less complex Driver, to understand why program won't work

--- killTeamList/AdMechDriver.java
+++ killTeamList/AdMechDriver.java
@@ -1,31 +1,19 @@
 package killTeamList;
 
 import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public class AdMechDriver {
 
-public static void main(String [] args) {
+	public static void main(String [] args) {
 		
-		Scanner question = new Scanner(System.in);
-		
-		System.out.println("You want a list?");
-		
-		String answer = question.nextLine();
-		
-		if(answer.toLowerCase() == "no") {
+		AdMech lst = new AdMech();
+	
+		ArrayList<String> list = lst.assignment();
 			
-			System.out.println("Ok bye!");
-		}
-		
-		else {
-			
-			ArrayList<String> list = AdMech.retrieve();
-			
-			for(int i = 0; i < list.size(); i++) {
+		for(int i = 0; i < list.size(); i++) {
 			
 				System.out.println(list.get(i));
-			}				
-		}
+		}				
 	}
 }
